18c0082961ec6b70c947bc170b1c84cc19b54634
tudo-bem-autismo <tbautismo22@gmail.com>
2022-08-31 16:57:54 -0300

    In progress

---
+++ main.js
@@ -0,0 +1,23 @@
+'use strict'
+
+function caracteristicas(){
+    document.getElementById("texto-dois-e-tres").innerHTML = "Uma criança no quadro do transtorno do espectro autista pode apresentar os seguintes sintomas:" 
+     + "<br> Ausência completa de qualquer contato interpessoal , como manter o contato visual, expressão facial e expressar os próprios sentimentos;"
+     + "<br> Dificuldade na comunicação, optando pelo uso repetitivo da linguagem e bloqueios para começar e manter um diálogo;"
+     + "<br> Alterações comportamentais, como manias, apego excessivo a rotinas, ações repetitivas e interesse intenso em coisas específicas.";
+}
+function diagnosticos(){
+    document.getElementById("texto-dois-e-tres").innerHTML = "Para obter o diagnóstico do Transtorno do Espectro Autista, é necessário a observação do comportamento "
+    + "<br> e do desenvolvimento pessoal. Para um diagnóstico mais rápido e eficaz, é importante procurar um especialista. Além da "
+    + "<br> observação do comportamento, alguns exames podem ser solicitados, como, exames de audição e sangue, teste de pezinho,"
+    + "<br> ressonância magnética nuclear, eletroencefalograma (EEG, e cariótipo com pesquisa de X frágil; ";
+}
+function tratamentos(){
+    document.getElementById("texto-dois-e-tres").innerHTML = "Com o avanço da psicologia, atualmente existem diferentes formas de ajudar pessoas com TEA. "
+    + "<br> O ABA, por exemplo, é uma terapia que consiste em ajudar a pessoa a como reagir em diferentes situações fora do ambiente terapêutico. O TEACHH "
+    + "<br> (Tratamento e Educaçãopara Autistas e Crianças com Déficits Relacionados à Comunicação), é um método para autismo de técnica comportamental, "
+    + "<br> visando a aprendizagem, tanto em casa como em ambiente escolar.";
+}
+
+
+
