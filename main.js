f947ac7b976b85b6026f42cf28136b67af7b6317
tudo-bem-autismo <tbautismo22@gmail.com>
2022-12-06 10:20:41 -0300

    Done

--- main.js
+++ main.js
@@ -14,9 +14,9 @@ function diagnosticos(){
 }
 function tratamentos(){
     document.getElementById("texto-dois-e-tres").innerHTML = "Com o avanço da psicologia, atualmente existem diferentes formas de ajudar crianças com TEA. "
-    + "<br> O ABA, por exemplo, é uma terapia que consiste em ajudar a pessoa a como reagir em diferentes situações fora do ambiente  "
+    + "<br> O ABA, por exemplo, é uma terapia que consiste em auxiliar a pessoa a como reagir em diferentes situações fora do ambiente  "
     + "<br> terapêutico. O TEACHH (Tratamento e Educaçãopara Autistas e Crianças com Déficits Relacionados à Comunicação), é um , "
-    + "<br> método para autismo de técnica comportamentalvisando a aprendizagem, tanto em casa como em ambiente escolar.";
+    + "<br> método para autismo de técnica comportamental visando a aprendizagem, tanto em casa como em ambiente escolar.";
 } 
 
 
